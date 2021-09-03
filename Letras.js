555dbde6e3fd68a3749d009f30a8ec95834cd414
Estefanya <estefanya.nascimento@outlook.com>
2021-09-02 21:58:10 -0300

    Semana 3

---
+++ Letras.js
@@ -0,0 +1,11 @@
+var letra = prompt('Digite uma letra:')
+
+if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') 
+{
+alert('letra digitada é uma vogal')
+} else {
+    alert('letra digitada é uma consoante')
+}
+
+
+
