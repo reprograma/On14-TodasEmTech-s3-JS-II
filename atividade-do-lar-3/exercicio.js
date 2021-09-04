ec2dedfa3e0bbfc84807483d98538cfb79ef9562
juliana-santana <juesantana@gmail.com>
2021-09-04 03:08:08 -0300

    atividade do lar - semana 3 -juliana

---
+++ atividade-do-lar-3/exercicio.js
@@ -0,0 +1,17 @@
+let letra = prompt('insira uma letra');
+
+switch(letra){ 
+    case "a":alert('Essa letra é uma vogal');
+    break;
+    case "e":alert('Essa letra é uma vogal');
+    break;
+    case "i":alert('Essa letra é uma vogal'); 
+    break;
+    case "o":alert('Essa letra é uma vogal');
+    break;
+    case "u":alert('Essa letra é uma vogal');
+    break;
+   
+    default:
+        alert('Essa letra é uma consoante')
+}
