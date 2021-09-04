d44e6e7fff4a2186ac0c344a87ebcfc2f57b41cc
Edilainesds <edilaine45@yahoo.com.br>
2021-09-04 18:05:10 -0300

    atividade feita

---
+++ Backendaulas/Farmacia.js
@@ -0,0 +1,16 @@
+// Atividade 1 - Farmácia JS está em promocao.
+// A cada 2 Produtos comprados iguais comprados, receba 5 reais de desconto.
+
+var Shampoo1 = Number(prompt("digite o valor do primeiro shampoo:"));
+alert("Aviso este shampoo esta em promocao comprando 2 voce ganha R$ 3,00 de desconto.");
+
+var Shampoo2 = Number(prompt("digite o valor do segundo shampoo:"));
+var desconto = 5 
+
+
+if (shampoo1 == shampoo2) {
+    alert("você ganhou R$ 3,00 de desconto. \n\nValor  valor a pagar: R$ " +(soma - desconto))
+} else {
+    
+}
+alert("você ganhou R$ 3,00 de desconto. \n\nValor  valor a pagar: R$ " +(soma - desconto))
---
+++ Backendaulas/exsemana3.js
@@ -0,0 +1,24 @@
+// Atividade 2 - // Receba uma letra pelo prompt e verifique se é uma vogal ou consoante.
+
+let vogal = prompt("insira uma letra:");
+
+switch (vogal) {
+    case "a":
+        alert("vogal correta");
+         break;
+    case "e":
+     alert("vogal correta");
+         break;
+    case "i":
+     alert("vogal correta");
+          break; 
+    case "o":
+     alert("vogal correta");
+          break;
+    case "u":
+     alert("vogal correta");
+          break;
+    default:
+        alert("você digitou uma consoante");
+        break;
+}
