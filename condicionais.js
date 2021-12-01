8d6f81b7acbe71d246eae05cb576524f88d8942b
Elcilene <elcilenematosaragao@gmail.com>
2021-11-30 23:35:20 -0300

    Subindo atividade

---
+++ condicionais.js
@@ -0,0 +1,19 @@
+var letras = prompt("Insira uma letra!")
+
+switch (letras){
+    case "a"|| "e"||"i"||"o"||"u": alert( "É uma vogal");
+    break;
+    default: alert("Sua resposta não está correta!")
+}
+
+
+let produto1 = Number(prompt("valor do produto?"))
+let produto2 = Number(prompt("valor do segundo produto?"))
+let desconto = 5
+
+soma = produto1 + produto2
+
+if (produto1 == produto2){ 
+    alert( valor = soma - desconto);
+}else{ alert(soma);
+}
